9e583198cfeccce0a97b19f9a41d6c34564fc830
Fovir <fovir@disroot.org>
2025-11-06 19:29:02 +0800

    feat: Add solution of greedy bfs.

--- 45.jump-game-ii.java
+++ 45.jump-game-ii.java
@@ -7,24 +7,19 @@ import java.util.*;
 // @leet start
 class Solution {
   public int jump(int[] nums) {
-    final int n = nums.length;
-    int[] steps = new int[n];
+    int jumps = 0;
+    int currentEnd = 0;
+    int currentFarthest = 0;
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 1; j <= nums[i]; j++) {
-        int current = i + j;
-        if (current >= n) {
-          continue;
-        }
-        steps[current] =
-            steps[current] == 0 ? steps[i] + 1 : Math.min(steps[current], steps[i] + 1);
-        if (steps[n - 1] != 0) {
-          return steps[n - 1];
-        }
+    for (int i = 0; i < nums.length - 1; i++) {
+      currentFarthest = Math.max(currentFarthest, nums[i] + i);
+      if (i == currentEnd) {
+        jumps++;
+        currentEnd = currentFarthest;
       }
     }
 
-    return steps[n - 1];
+    return jumps;
   }
 }
 // @leet end
