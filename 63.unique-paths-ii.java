a6255d4d811ac5014b19568fe15552279bd1c372
Fovir <fovir@disroot.org>
2025-11-10 11:12:57 +0800

    feat: Solve `unique-paths-ii` with 1-d array.

--- 63.unique-paths-ii.java
+++ 63.unique-paths-ii.java
@@ -7,22 +7,21 @@ import java.util.*;
 // @leet start
 class Solution {
   public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    int m = obstacleGrid.length;
     int n = obstacleGrid[0].length;
-    int dp[][] = new int[m + 1][n + 1];
+    int dp[] = new int[n];
+    dp[0] = 1;
 
-    for (int i = 1; i <= m; i++) {
-      for (int j = 1; j <= n; j++) {
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-        if (obstacleGrid[i - 1][j - 1] == 1) {
-          dp[i][j] = 0;
-        }
-        if (i == 1 && j == 1) {
-          dp[i][j] = obstacleGrid[i - 1][j - 1] == 0 ? 1 : 0;
+    for (int[] row : obstacleGrid) {
+      for (int i = 0; i < n; i++) {
+        if (row[i] == 1) {
+          dp[i] = 0;
+        } else if (i > 0) {
+          dp[i] += dp[i - 1];
         }
       }
     }
-    return dp[m][n];
+
+    return dp[n - 1];
   }
 }
 // @leet end
