ffdf646a4cb7c2c03e9ae28d9c6f22ff1cdd8513
Fovir <fovir@disroot.org>
2025-11-10 11:09:30 +0800

    feat: Solve `unique-paths-ii`.

---
+++ 63.unique-paths-ii.java
@@ -0,0 +1,28 @@
+// @leet imports start
+import java.math.*;
+import java.util.*;
+
+// @leet imports end
+
+// @leet start
+class Solution {
+  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    int m = obstacleGrid.length;
+    int n = obstacleGrid[0].length;
+    int dp[][] = new int[m + 1][n + 1];
+
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        if (obstacleGrid[i - 1][j - 1] == 1) {
+          dp[i][j] = 0;
+        }
+        if (i == 1 && j == 1) {
+          dp[i][j] = obstacleGrid[i - 1][j - 1] == 0 ? 1 : 0;
+        }
+      }
+    }
+    return dp[m][n];
+  }
+}
+// @leet end
