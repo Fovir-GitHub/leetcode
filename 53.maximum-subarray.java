7c5c9a427b9fa2d8cb2541eee7ba2e9089d46b13
Fovir <fovir@disroot.org>
2025-11-10 09:49:27 +0800

    feat: Optimize with rolling array.

--- 53.maximum-subarray.java
+++ 53.maximum-subarray.java
@@ -8,13 +8,14 @@ import java.util.*;
 class Solution {
   public int maxSubArray(int[] nums) {
     int n = nums.length;
-    int dp[] = new int[n];
-    dp[0] = nums[0];
+    int previous = nums[0];
+    int current = 0;
     int maxSum = nums[0];
 
     for (int i = 1; i < n; i++) {
-      dp[i] = nums[i] + Math.max(dp[i - 1], 0);
-      maxSum = Math.max(maxSum, dp[i]);
+      current = nums[i] + Math.max(previous, 0);
+      maxSum = Math.max(current, maxSum);
+      previous = current;
     }
 
     return maxSum;
